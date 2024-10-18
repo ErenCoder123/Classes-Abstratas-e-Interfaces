3760320ea48a69760c8737d26cc267119ac786d3
Miguel Cardoso <migueltranslatorbr@gmail.com>
2024-10-17 23:18:14 -0300

    commitando

---
+++ Bicicleta.cs
@@ -0,0 +1,16 @@
+public class Bicicleta : Veiculo, IAceleravel
+{
+    public Bicicleta(string marca, string modelo):base(marca, modelo)
+    {
+        
+    }
+    public override void Ligar()
+    {
+        Console.WriteLine("A bicicleta está ligada.");
+    }
+    public void Acelerar()
+    {
+        Console.WriteLine("A bicicleta esta pedalando mais rapido.");
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿var carro = new Carro("Tesla", "CyberTruck");
+carro.Ligar();
+carro.Acelerar();
+carro.Frear();
+carro.Desligar();
+Console.WriteLine();
+
+var bicicleta = new Bicicleta("South", "BMX");
+bicicleta.Ligar();
+bicicleta.Acelerar();
+bicicleta.Desligar();
